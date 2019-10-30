a71b043ae284a2343fd9eaede7c1a69b95e92610
rhian <rhian.costa>
2019-10-30 16:57:10 -0300

    feat: add timeline screen at app

---
+++ app/app/src/main/java/com/example/mypet/Post.java
@@ -0,0 +1,27 @@
+package com.mypet.MyPet.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class Post extends Domain{
+
+    private Pet pet;
+
+    private ArrayList<Comment> comment;
+
+    private int amountEnjoy;
+
+    private String photos;
+
+    private String description;
+
+        private LocalDateTime dateTime;
+}
---
+++ app/app/src/main/java/com/example/mypet/PostService.java
@@ -0,0 +1,4 @@
+package com.example.mypet;
+
+public interface PostService {
+}
